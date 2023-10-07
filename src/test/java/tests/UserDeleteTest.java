29f6782e072588f555bad011b5f0f6cb46129478
horo053 <yulfi555@gmail.com>
2023-10-07 23:09:29 +0300

    add files with Delete tests

---
+++ src/test/java/tests/UserDeleteTest.java
@@ -0,0 +1,130 @@
+package tests;
+
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import lib.Assertions;
+import lib.BaseTestCase;
+import lib.DataGenerater;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserDeleteTest extends BaseTestCase {
+    @Test
+    public void testDeleteBlockedUserTest() {
+        //LOGIN
+        Map<String, String> authData = new HashMap<>();
+        authData.put("email", "vinkotov@example.com");
+        authData.put("password", "1234");
+
+        Response responseGetAuth = RestAssured
+                .given()
+                .body(authData)
+                .post("https://playground.learnqa.ru/api/user/login")
+                .andReturn();
+
+        //DELETE
+        Map<String, String> editData = new HashMap<>();
+
+        Response responseDeleteUser = RestAssured
+                .given()
+                .header("x-csrf-token", this.getHeader(responseGetAuth, "x-csrf-token"))
+                .cookie("auth_sid", this.getCookie(responseGetAuth, "auth_sid"))
+                .body(editData)
+                .delete("https://playground.learnqa.ru/api/user/2")
+                .andReturn();
+
+        Assertions.assertResponseCodeEquals(responseDeleteUser, 400);
+        Assertions.assertResponseTextEquals(responseDeleteUser, "Please, do not delete test users with ID 1, 2, 3, 4 or 5.");
+    }
+
+    @Test
+    public void testDeletePositiveTest() {
+        //GENERATE USER
+        Map<String, String> userData = DataGenerater.getRegistrationData();
+
+        JsonPath responseCreateAuth = RestAssured
+                .given()
+                .body(userData)
+                .post("https://playground.learnqa.ru/api/user/")
+                .jsonPath();
+
+        String userId = responseCreateAuth.getString("id");
+
+        //LOGIN
+        Map<String, String> authData = new HashMap<>();
+        authData.put("email", userData.get("email"));
+        authData.put("password", userData.get("password"));
+
+        Response responseGetAuth = RestAssured
+                .given()
+                .body(authData)
+                .post("https://playground.learnqa.ru/api/user/login")
+                .andReturn();
+
+        //DELETE
+        Map<String, String> editData = new HashMap<>();
+
+        Response responseDeleteUser = RestAssured
+                .given()
+                .header("x-csrf-token", this.getHeader(responseGetAuth, "x-csrf-token"))
+                .cookie("auth_sid", this.getCookie(responseGetAuth, "auth_sid"))
+                .body(editData)
+                .delete("https://playground.learnqa.ru/api/user/" + userId)
+                .andReturn();
+
+        Assertions.assertResponseCodeEquals(responseDeleteUser, 200);
+
+        //GET
+        Response responseUserData = RestAssured
+                .given()
+                .header("x-csrf-token", this.getHeader(responseGetAuth, "x-csrf-token"))
+                .cookie("auth_sid", this.getCookie(responseGetAuth, "auth_sid"))
+                .get("https://playground.learnqa.ru/api/user/" + userId)
+                .andReturn();
+
+        Assertions.assertResponseCodeEquals(responseUserData, 404);
+        Assertions.assertResponseTextEquals(responseUserData, "User not found");
+    }
+
+    @Test
+    public void testDeleteAnotherUser(){
+        //GENERATE USER
+        Map<String, String> userData = DataGenerater.getRegistrationData();
+
+        JsonPath responseCreateAuth = RestAssured
+                .given()
+                .body(userData)
+                .post("https://playground.learnqa.ru/api/user/")
+                .jsonPath();
+
+        String userId = responseCreateAuth.getString("id");
+
+        //LOGIN
+        Map<String, String> authData = new HashMap<>();
+        authData.put("email", "vinkotov@example.com");
+        authData.put("password", "1234");
+
+        Response responseGetAuth = RestAssured
+                .given()
+                .body(authData)
+                .post("https://playground.learnqa.ru/api/user/login")
+                .andReturn();
+
+        //DELETE
+        Map<String, String> editData = new HashMap<>();
+
+        Response responseDeleteUser = RestAssured
+                .given()
+                .header("x-csrf-token", this.getHeader(responseGetAuth, "x-csrf-token"))
+                .cookie("auth_sid", this.getCookie(responseGetAuth, "auth_sid"))
+                .body(editData)
+                .delete("https://playground.learnqa.ru/api/user/" + userId)
+                .andReturn();
+
+        Assertions.assertResponseCodeEquals(responseDeleteUser, 400);
+        Assertions.assertResponseTextEquals(responseDeleteUser, "Please, do not delete test users with ID 1, 2, 3, 4 or 5.");
+    }
+}
